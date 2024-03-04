65fbdb34db232a4cc4842179fbd82c8ce7e1ac37
Zaur <152211771+salamjs@users.noreply.github.com>
2024-03-04 10:16:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-const checkbox = document.getElementById('checkbox');
+const checkbox = document.getElementById("checkbox");
 
 const checkSavedTheme = () => {
-  const theme = localStorage.getItem('theme');
+  const theme = localStorage.getItem("theme");
+  document.documentElement.setAttribute(
+    "data-theme", theme || "light"
+  );
 
-  if (theme) {
-    document
-      .documentElement
-      .setAttribute('data-theme', theme)
-
-    if (theme === 'dark') {
-      checkbox.checked = true
-    }
+  if (theme === "dark") {
+    checkbox.checked = true;
+  } else {
+    checkbox.checked = false;
   }
-}
+};
 
 const switchTheme = (e) => {
   if (e.target.checked) {
-    document
-      .documentElement
-      .setAttribute('data-theme', 'dark')
-    localStorage.setItem('theme', 'dark')
-    return
+    document.documentElement.setAttribute(
+      "data-theme", "dark"
+    );
+    localStorage.setItem("theme", "dark");
+    return;
   }
 
-  document
-    .documentElement
-    .setAttribute('data-theme', 'light');
+  document.documentElement.setAttribute(
+    "data-theme", "light"
+  );
+  localStorage.setItem("theme", "light");
+};
 
-  localStorage.setItem('theme', 'light')
-}
-
-checkSavedTheme()
+checkSavedTheme();
 checkbox.onchange = switchTheme;
+window.onstorage = checkSavedTheme;
